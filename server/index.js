7bf3bc4aec672cb752ae618aad25024eef75e695
RafaSaan <rafasaanchez@icloud.com>
2024-03-05 10:26:04 -0600

    update server conection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
 import express from "express";
 import http from "http";
-// import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
-// import { resolve, dirname } from "path";
-
-// import { PORT } from "./config.js";
-import cors from "cors";
 
 // Initializations
 const app = express();
@@ -17,19 +12,12 @@ const io = new SocketServer(server, {
 });
 const PORT = 3000;
 
-// Middlewares
-// app.use(cors());
-// app.use(morgan("dev"));
-// app.use(express.urlencoded({ extended: false }));
-
-// app.use(express.static(resolve("frontend/dist")));
-
 io.on("connection", (socket) => {
   console.log(socket.id);
   socket.on("message", (body) => {
     socket.broadcast.emit("message", {
       body,
-      from: socket.id.slice(8),
+      from: socket.id.slice(15),
     });
   });
 });
